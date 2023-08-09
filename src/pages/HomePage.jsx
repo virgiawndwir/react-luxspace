16b1bf1f756b27e318de8712c7c909e40b6a590f
Muhamad Virgiawan Dwi Rangga <virgiawndwir@gmail.com>
2023-08-09 17:21:00 +0700

    add scroll & modal youtube

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 import Header from '../parts/Header' 
-import Hero from '../parts/Hero'
+import Hero from '../parts/HomePage/Hero'
 import JustArrived from '../parts/HomePage/JustArrived'
 import BrowseTheRoom from '../parts/HomePage/BrowseTheRoom'
 import Clients from '../parts/Clients'
 import SiteMap from '../parts/SiteMap'
 import Footer from '../parts/Footer'
+import useScrollAnchor from '../helpers/hooks/useScrollAnchor'
+import useModalDOM from '../helpers/hooks/useModalDOM'
 
 export default function HomePage(props) {
+  useScrollAnchor()
+  useModalDOM()
   return (
     <>
         <Header theme="white" position="absolute" />
